27f7652b19634d3f9e538444c08d32d5d963500f
kiwoon Kim <87232579+tigerenergy@users.noreply.github.com>
2021-09-01 17:36:45 +0900

    Add files via upload

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,6 +1,13 @@
     precision mediump float;
 
+    uniform vec3 uColor;
+    uniform sampler2D uTexture;
+
+    varying vec2 vUv;
+
+
     void main()
-    {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    {   
+        vec4 textureColor = texture2D(uTexture, vUv);
+        gl_FragColor = textureColor;
     }
